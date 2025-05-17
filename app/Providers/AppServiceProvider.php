52b1b266ec185997f18dd7f16769208579ba7388
farhan12112004 <farhansadiqalfaritzimunthe@gmail.com>
2025-05-18 02:10:35 +0700

    ketiga

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+       if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && 
+        strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
         \URL::forceScheme('https');
+    }
     }
 }
